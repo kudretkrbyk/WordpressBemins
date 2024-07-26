b60b861d1c2ca697a91c245f1824b5454d459d5f
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-26 23:37:51 +0300

    added style categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,46 @@
 export default function Categories() {
   return (
-    <div>
-      <div>
+    <div className="w-full p-4">
+      <div className="w-full flex items-center justify-center">
         <span>Shop By Categories</span>
       </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex items-center justify-center w-full gap-10">
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category.jpg"
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-4.jpg"
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-2.jpg"
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-5.jpg"
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-3.jpg"
+          ></img>
+        </div>
+        <div>
+          <img
+            className="rounded-full"
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-1.jpg"
+          ></img>
+        </div>
       </div>
     </div>
   );
