f10902f2e533122f49803251bcbc0893be267387
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-26 23:19:36 +0300

    Banner added image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,15 @@
 export default function Banner() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/banner.jpg"></img>
+      </div>
+      <div>
+        <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/banner-1.jpg"></img>
+      </div>
+      <div>
+        <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/banner-2.jpg"></img>
+      </div>
+    </div>
+  );
 }
