53fb30490e9d9e7c893caa781d1a98d7644c092a
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-26 23:33:29 +0300

    .

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,17 @@
 export default function Categories() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <span>Shop By Categories</span>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
