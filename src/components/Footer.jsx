5da7b91bd2a7b1919b4c08436528c4dec52da5ce
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-28 19:47:43 +0300

    footer added menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,47 @@
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen flex flex-col gap-10 p-10">
+      <div className="grid grid-rows-4">
+        <div>
+          <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2020/06/logo.png"></img>
+          <div>
+            Contact our customer happiness team Monday-Friday 9am-5pm (02) 6188
+            8062
+          </div>
+        </div>
+        <div>
+          <div>Shop</div>
+          <div>
+            {" "}
+            <div>Shop dog</div>
+            <div>Shop cat</div>
+            <div>Shop brands</div>
+            <div>On sale</div>
+            <div>Auto-Delivery</div>
+            <div>Aussie Hazard Cover</div>
+          </div>
+        </div>
+        <div>
+          <div>Shop</div>
+          <div>
+            {" "}
+            <div>Shop dog</div>
+            <div>Shop cat</div>
+            <div>Shop brands</div>
+            <div>On sale</div>
+            <div>Auto-Delivery</div>
+            <div>Aussie Hazard Cover</div>
+          </div>
+        </div>
+        <div>
+          <div>Stay in touch...</div>
+          <div>
+            Subscribe and be the first to know about exclusive offers, products,
+            promotions & more
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
