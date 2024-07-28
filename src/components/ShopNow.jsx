2269d392b99e87784b189356d281b45bece6f23f
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-28 19:40:24 +0300

    added style

--- src/components/ShopNow.jsx
+++ src/components/ShopNow.jsx
@@ -7,10 +7,10 @@ export default function ShopNow() {
         <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/video.jpg"></img>
       </div>
       <div className="absolute w-full h-screen flex flex-col items-center justify-center gap-10 z-20">
-        <div>
+        <div className="text-black hover:text-white hover:bg-[#54d9e1] size-20 rounded-full flex items-center justify-center duration-500">
           <FaPlay className="size-10" />
         </div>
-        <div className="text-5xl font-bold">Step into Fashion Excellence</div>
+        <div className="text-6xl font-bold">Step into Fashion Excellence</div>
         <div>Unveiling the Latest Trends and Must-Have Looks</div>
         <div className="p-5">
           <button className="border border-black p-3 px-5 rounded-xl hover:bg-[#54d9e1] hover:border-white hover:text-white duration-500 ">
