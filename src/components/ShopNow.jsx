dc86601536b11b9ab514b3ec44d2a48d76f42091
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-28 19:38:13 +0300

    added style

--- src/components/ShopNow.jsx
+++ src/components/ShopNow.jsx
@@ -1,3 +1,5 @@
+import { FaPlay } from "react-icons/fa";
+
 export default function ShopNow() {
   return (
     <div className="w-full h-screen relative">
@@ -5,11 +7,15 @@ export default function ShopNow() {
         <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/video.jpg"></img>
       </div>
       <div className="absolute w-full h-screen flex flex-col items-center justify-center gap-10 z-20">
-        <div>d111</div>
-        <div>Step into Fashion Excellence</div>
-        <div>Unveiling the Latest Trends and Must-Have Looks</div>
         <div>
-          <button className="border border-black p-3 px-4 ">Shop Now</button>
+          <FaPlay className="size-10" />
+        </div>
+        <div className="text-5xl font-bold">Step into Fashion Excellence</div>
+        <div>Unveiling the Latest Trends and Must-Have Looks</div>
+        <div className="p-5">
+          <button className="border border-black p-3 px-5 rounded-xl hover:bg-[#54d9e1] hover:border-white hover:text-white duration-500 ">
+            Shop Now
+          </button>
         </div>
       </div>
     </div>
