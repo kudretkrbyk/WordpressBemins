e1e3ec387574e838699117af62be693011def294
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-30 23:44:50 +0300

    added style

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,58 +1,86 @@
+import { useEffect, useState } from "react";
+
 export default function Shop() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch("/src/data/products.json")
+      .then((response) => response.json())
+      .then((data) => setProductList(data))
+      .catch((error) => console.error("Error fetching JSON:", error));
+  }, []);
+
   return (
     <div className="w-full  flex flex-col  ">
       <div className="w-full h-[480px] relative border border-red-500 overflow-hidden">
         <div className="absolute">
-          <img src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/lookbook-3.jpg"></img>
+          <img
+            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/lookbook-3.jpg"
+            alt="Lookbook"
+          />
         </div>
         <div className="absolute flex items-center justify-center w-full gap-10 bottom-0">
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category.jpg"
-            ></img>
+              alt="Tops"
+            />
             <span>Tops</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-4.jpg"
-            ></img>
+              alt="Shirts"
+            />
             <span>Shirts</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-2.jpg"
-            ></img>
+              alt="Glasses"
+            />
             <span>Glasses</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-5.jpg"
-            ></img>
+              alt="Sandals"
+            />
             <span>Sandals</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-3.jpg"
-            ></img>
+              alt="Bag"
+            />
             <span>Bag</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <img
               className="rounded-full hover:scale-90 duration-700"
               src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-1.jpg"
-            ></img>
+              alt="Shoes"
+            />
             <span>Shoes</span>
           </div>
         </div>
       </div>
       <div className="w-full flex">
         <div className="bg-red-500 w-3/12 h-full">dd</div>
-        <div className="bg-blue-500 w-9/12 h-full">dd</div>
+        <div className=" w-9/12 h-full">
+          <div className="grid grid-cols-3 gap-4">
+            {productList.map((product, index) => (
+              <div key={index}>
+                <img src={product.fotograflar[0]}></img>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
