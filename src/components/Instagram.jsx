7b394102ee1494426e554bb50f7f10c41f2c94a5
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-16 11:37:18 +0300

    added feeds

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Instagram() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const feeds = [
+    {
+      id: 1,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram-1.jpg",
+    },
+    {
+      id: 2,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram-2.jpg",
+    },
+    {
+      id: 3,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram-3.jpg",
+    },
+    {
+      id: 4,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram-4.jpg",
+    },
+    {
+      id: 5,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram-5.jpg",
+    },
+    {
+      id: 6,
+      image:
+        "https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/12/instagram.jpg",
+    },
+  ];
   return <div></div>;
 }
