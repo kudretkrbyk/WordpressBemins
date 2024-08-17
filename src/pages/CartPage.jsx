c0ba4459db59e857e019bf910f0823af8e23ec13
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-17 22:29:22 +0300

    added input button

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -16,9 +16,9 @@ export default function CartPage() {
   console.log("burası cart");
   return (
     <div className="w-full h-screen flex items-center justify-center gap-10 p-10">
-      <div className="flex flex-col gap-10 w-9/12 ">
-        <div className="flex items-center justify-between w-full ">
-          <div>Product</div>
+      <div className="flex flex-col gap-10 w-9/12 border ">
+        <div className="flex items-center justify-between w-full border-b p-4">
+          <div className="">Product</div>
           <div className="w-full flex items-center justify-end gap-20 ">
             <div>Price</div>
             <div>Quantity</div>
@@ -38,13 +38,33 @@ export default function CartPage() {
             </div>
           </div>
         ))}
+        <div>
+          <div className="p-4 flex items-center justify-between">
+            <div className="flex items-center justify-center gap-3">
+              <input
+                className="border focus:outline-none w-60 h-10 rounded px-2"
+                placeholder="Coupon code"
+                type="input"
+              ></input>
+              <button className="bg-black text-white p-2 px-6 rounded">
+                Apply Coupon
+              </button>
+            </div>
+
+            <div>
+              <button className="border border-black rounded p-2 px-6">
+                Continue Shopping
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col w-3/12">
-        <div className="bg-gray-700 p-5"> Cart Totals</div>
-        <div className="flex flex-col bg-gray-300 p-5 gap-10">
+        <div className="bg-[#e9e9e9] p-5 font-bold"> Cart Totals</div>
+        <div className="flex flex-col bg-[#f6f6f6] p-5 gap-10">
           <div className="flex items-center justify-between gap-40 ">
             <div>Sub total</div>
-            <div>111</div>
+            <div className="font-bold">111</div>
           </div>
           <div className="flex items-center justify-between gap-40 ">
             <div>Shipping</div>
@@ -53,7 +73,12 @@ export default function CartPage() {
           <div className="flex items-center justify-between gap-40 ">
             {" "}
             <div>Total</div>
-            <div>{totalCost} </div>
+            <div className="font-bold">${totalCost} </div>
+          </div>
+          <div>
+            <button className="text-white bg-black p-4 px-20 rounded hover:bg-[#54d9e1] duration-300">
+              Proceed to checkout
+            </button>
           </div>
         </div>
       </div>
