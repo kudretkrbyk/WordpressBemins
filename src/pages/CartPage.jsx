55a2409252e6748004985fc1b1a51563fdc9f19f
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-17 22:39:30 +0300

    added subtotalcost and totalcost

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,14 +4,15 @@ import { useState } from "react";
 export default function CartPage() {
   const cartItems = useSelector((state) => state.cart.cartItems);
 
-  const handleCalculateTotalCost = () => {
+  const handleCalculateSubTotalCost = () => {
     const totalCost = cartItems.reduce((total, item) => {
       return total + item.fiyat * item.quantity; // Her ürünün fiyatı ve miktarını çarparak toplama ekler
     }, 0); // Başlangıç değeri 0
 
     return totalCost;
   };
-  const totalCost = handleCalculateTotalCost();
+  const SubTotalCost = handleCalculateSubTotalCost();
+  const totalCost = SubTotalCost + (SubTotalCost * 18) / 100;
 
   console.log("burası cart");
   return (
@@ -27,32 +28,39 @@ export default function CartPage() {
         </div>
         {cartItems.map((items, index) => (
           <div key={index} className="flex items-center justify-between ">
-            <div className="flex items-center gap-3 w-full">
-              <img className="w-[200px] h-[200px]" src={items.fotograflar[0]} />
+            <div className="flex items-center gap-3 w-full ">
+              <img
+                className="w-[200px] h-[200px] object-contain object-center"
+                src={items.fotograflar[0]}
+              />
               <div>{items.ad} </div>
             </div>
-            <div className="flex items-start justify-end gap-24 w-full">
+            <div className="flex items-start justify-end gap-16 w-full p-4">
               <div>{items.fiyat} </div>
-              <div>{items.quantity} </div>
+              <div className="flex items-center justify-center border gap-10 p-2">
+                <span className="font-bold ">+</span>
+                <span>{items.quantity}</span>
+                <span className="font-bold ">-</span>
+              </div>
               <div>${items.fiyat} </div>
             </div>
           </div>
         ))}
         <div>
-          <div className="p-4 flex items-center justify-between">
+          <div className="p-4 flex items-center justify-between ">
             <div className="flex items-center justify-center gap-3">
               <input
-                className="border focus:outline-none w-60 h-10 rounded px-2"
+                className="border focus:outline-none w-80 h-10 rounded px-2 "
                 placeholder="Coupon code"
                 type="input"
               ></input>
-              <button className="bg-black text-white p-2 px-6 rounded">
+              <button className="bg-black text-white p-2 px-6 rounded hover:bg-[#54d9e1] duration-300">
                 Apply Coupon
               </button>
             </div>
 
             <div>
-              <button className="border border-black rounded p-2 px-6">
+              <button className="border border-black rounded p-2 px-6 hover:bg-[#54d9e1] duration-300">
                 Continue Shopping
               </button>
             </div>
@@ -64,11 +72,11 @@ export default function CartPage() {
         <div className="flex flex-col bg-[#f6f6f6] p-5 gap-10">
           <div className="flex items-center justify-between gap-40 ">
             <div>Sub total</div>
-            <div className="font-bold">111</div>
+            <div className="font-bold">${SubTotalCost}</div>
           </div>
           <div className="flex items-center justify-between gap-40 ">
             <div>Shipping</div>
-            <div>asdasd</div>
+            <div>Turkey</div>
           </div>
           <div className="flex items-center justify-between gap-40 ">
             {" "}
