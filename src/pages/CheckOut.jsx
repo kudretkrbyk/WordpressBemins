a92be1627ed0c001d1b7a7c0b291a64b9b689935
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-18 22:58:08 +0300

    added style and button

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,5 +1,7 @@
 import { GoPerson } from "react-icons/go";
 import { BiSolidDiscount } from "react-icons/bi";
+import { GoDotFill, GoDot } from "react-icons/go";
+
 import { useSelector } from "react-redux";
 import ChectOutForm from "../components/ChectOutForm";
 import CartPageNavigator from "../components/CartPageNavigator";
@@ -41,7 +43,7 @@ export default function CheckOut() {
       </div>
       <div className="flex items-center justify-center gap-4 w-full p-10">
         <ChectOutForm></ChectOutForm>
-        <div className="w-2/5 h-full flex flex-col gap-4 border border-black p-2">
+        <div className="w-2/5 h-full flex flex-col gap-4 border border-black p-10">
           <div>Product</div>
           <div className="flex flex-col gap-2">
             {" "}
@@ -78,13 +80,44 @@ export default function CheckOut() {
           <div className="bg-black w-full h-[1px]"></div>
           <div className="w-full flex items-center justify-between">
             <div>Shipping</div>
-            <div className="text-2xl"> Flat rate</div>
+            <div className="text-2xl flex items-center">
+              <GoDotFill className="size-7" /> <span> Flat rate</span>
+            </div>
           </div>
           <div className="bg-black w-full h-[1px]"></div>
 
           <div className="w-full flex items-center justify-between">
             <div>Total</div>
             <div className="text-2xl"> ${totalCost}</div>
+          </div>
+
+          <div className="flex flex-col gap-5 border border-black p-3">
+            <div className="flex items-center">
+              <GoDotFill className="size-7" />
+              <span> Direct bank transfer</span>
+            </div>
+            <div>
+              Make your payment directly into our bank account. Please use your
+              Order ID as the payment reference. Your order will not be shipped
+              until the funds have cleared in our account.
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center gap-1">
+                <GoDot className="size-5" /> <span>Chech Payments</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <GoDot className="size-5" /> <span>Cash on delivery</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <GoDot className="size-5" /> <span>Paypal</span>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <button className="w-full bg-black text-white p-2 rounded hover:bg-[#54d9e1] duration-300">
+              Place Order
+            </button>
           </div>
         </div>
       </div>
