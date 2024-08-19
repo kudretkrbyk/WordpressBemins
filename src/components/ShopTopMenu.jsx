88d0530dedde455965596c088d48791f3f573d12
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-19 13:24:37 +0300

    added handleProductClick

--- src/components/ShopTopMenu.jsx
+++ src/components/ShopTopMenu.jsx
@@ -1,20 +1,20 @@
 export default function ShopTopMenu() {
+  const handleProductClick = (categoryName) => {
+    navigate(`/shop/${categoryName}`);
+  };
   return (
     <div className="w-full h-[480px] relative ">
-      <div className="absolute">
-        <img
-          src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/lookbook-3.jpg"
-          alt="Lookbook"
-        />
-      </div>
       <div className="absolute flex items-center justify-center w-full gap-10 bottom-0">
-        <div className="flex flex-col items-center justify-center gap-2">
+        <div
+          onClick={handleProductClick}
+          className="flex flex-col items-center justify-center gap-2"
+        >
           <img
             className="rounded-full hover:scale-90 duration-700"
             src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category.jpg"
             alt="Tops"
           />
-          <span>Tops</span>
+          <span onClick={handleProductClick}>Tops</span>
         </div>
         <div className="flex flex-col items-center justify-center gap-2">
           <img
