b832c60b686848a8283ed5e78ba2b3ac813b9695
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-19 15:47:08 +0300

    added style categories comp

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,51 +1,71 @@
+import { useNavigate } from "react-router-dom";
+import useFetchProducts from "../hooks/useFetchProducts";
+import Slider from "react-slick";
+
+import "../slick-carousel/slick.css";
+import "../slick-carousel/slick-theme.css";
+import PropTypes from "prop-types";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 export default function Categories() {
+  const { categories } = useFetchProducts();
+  const navigate = useNavigate();
+
+  const handleProductClick = (categoryName) => {
+    console.log("tıklama oldu");
+    navigate(`/shop/${categoryName}`);
+  };
+  const PrevArrow = ({ onClick }) => (
+    <IoIosArrowBack
+      style={{
+        zIndex: "1",
+      }} // Stil ve konum ayarları
+      onClick={onClick} // Tıklama işlevi
+      className="size-8 hover:cursor-pointer top-1/2 left-2 border border-black absolute rounded-full hover:bg-[#54d9e1] hover:text-white hover:border-none duration-300" // Varsayılan sınıf adları
+    />
+  );
+  const NextArrow = ({ onClick }) => (
+    <IoIosArrowForward
+      onClick={onClick}
+      className="size-8 hover:cursor-pointer top-1/2 right-2 border border-black absolute rounded-full hover:bg-[#54d9e1] hover:text-white hover:border-none duration-300 "
+    ></IoIosArrowForward>
+  );
+  PrevArrow.propTypes = NextArrow.propTypes = {
+    onClick: PropTypes.func, // onClick'in bir fonksiyon olduğunu belirtir
+  };
+  const settings = {
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    className: "react__slick__slider__parent",
+  };
   return (
     <div className="w-full p-4 flex flex-col">
       <div className="w-full flex items-center justify-center">
         <span>Shop By Categories</span>
       </div>
       <div className="flex items-center justify-center w-full gap-10">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category.jpg"
-          ></img>
-          <span>Tops</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-4.jpg"
-          ></img>
-          <span>Shirts</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-2.jpg"
-          ></img>
-          <span>Glasses</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-5.jpg"
-          ></img>
-          <span>Sandals</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-3.jpg"
-          ></img>
-          <span>Bag</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-1.jpg"
-          ></img>
-          <span>Shoes</span>
+        <div className="  w-full z-50 ">
+          <Slider {...settings}>
+            {categories.map((category, index) => (
+              <div
+                onClick={() => handleProductClick(category.name)}
+                key={index}
+                className="w-full"
+              >
+                <div className="flex flex-col items-center justify-center  w-full">
+                  {" "}
+                  <img
+                    className="rounded-full  hover:scale-90 duration-700  "
+                    src={category.image}
+                    alt={category.name}
+                  />
+                  <span>{category.name} </span>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
