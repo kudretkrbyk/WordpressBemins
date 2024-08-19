c7c22ee9fa38de2cc3c250898cd6ad464d36ef1f
kudretkrbyk <kudretkrbyk@gmail.com>
2024-08-19 15:19:32 +0300

    added category.map

--- src/components/ShopTopMenu.jsx
+++ src/components/ShopTopMenu.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import useFetchProducts from "../hooks/useFetchProducts";
 export default function ShopTopMenu() {
+  const { categories } = useFetchProducts();
   const navigate = useNavigate();
 
   const handleProductClick = (categoryName) => {
@@ -15,60 +17,20 @@ export default function ShopTopMenu() {
         />
       </div>
       <div className="absolute flex items-center justify-center w-full gap-10 bottom-0">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category.jpg"
-            alt="Tops"
-          />
-          <span onClick={() => handleProductClick("tops")}>Tops</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-4.jpg"
-            alt="Shirts"
-          />
-          <span onClick={() => handleProductClick("tops")}> Shirts</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-2.jpg"
-            alt="Glasses"
-          />
-          <span onClick={() => handleProductClick("tops")}>Glasses</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-5.jpg"
-            alt="Sandals"
-          />
-          <span onClick={() => handleProductClick("tops")}>Sandals</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-3.jpg"
-            alt="Bag"
-          />
-          <span onClick={() => handleProductClick("tops")}>Bag</span>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img
-            onClick={() => handleProductClick("tops")}
-            className="rounded-full hover:scale-90 duration-700"
-            src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2023/11/category-1.jpg"
-            alt="Shoes"
-          />
-          <span onClick={() => handleProductClick("tops")}>Shoes</span>
-        </div>
+        {categories.map((category, index) => (
+          <div
+            onClick={() => handleProductClick(category.name)}
+            key={index}
+            className="flex flex-col items-center justify-center gap-2"
+          >
+            <img
+              className="rounded-full hover:scale-90 duration-700"
+              src={category.image}
+              alt={category.name}
+            />
+            <span>{category.name} </span>
+          </div>
+        ))}
       </div>
     </div>
   );
