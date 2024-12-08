53da1f817901014aac6e819d0d952471e1f22fd2
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-08 20:06:13 +0300

    updated profile pics

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -29,7 +29,7 @@ export default function Profile({ profileOpen, setProfileOpen }) {
               <div className="relative z-50">
                 <img
                   className="object-cover object-center w-full h-full"
-                  src="https://wpbingosite.com/wordpress/bemins/wp-content/uploads/2024/02/sing-in.jpg"
+                  src="https://bemins.wpbingosite.com/wp-content/uploads/2024/02/sing-in.jpg"
                   alt="Sign In"
                 />
                 <div className="absolute bottom-0 left-0 p-7 text-2xl">
