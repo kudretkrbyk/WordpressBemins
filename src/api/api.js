8ea6870b787d0c5488c9eaff07eab24b94acf9d2
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-11 11:25:00 +0300

    .

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 
 export async function fetchProducts() {
   try {
-    const response = await fetch("../public/data/products.json");
+    const response = await fetch("/data/products.json");
     const data = await response.json();
     return data;
   } catch (error) {
